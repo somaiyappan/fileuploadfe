4336960ac2d92cfe9781b0022a4a0836d140ad18
somaiyappan <somasundaram9997@gmail.com>
2022-06-21 17:12:14 +0530

    file upload FE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import { Button, Stack, Typography } from "@mui/material";
+import React from "react"
+import axios from "axios"
+
 
 function App() {
+
+  const [file, setFile] = React.useState("")
+  const [filename, setFilename] = React.useState("")
+  const [text, setText] = React.useState("")
+
+
+
+  const handleInputChange = (e) => {
+    setFile(e.target.files[0]);
+
+
+    setFilename(e.target.files[0].name);
+
+
+  }
+  const handleButtonClick = () => {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('name', text);
+    
+
+
+
+
+    axios.post("http://192.168.1.27:3007/api/fileupload", formData).then((result) => {
+      console.log(result.data)
+    }).catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <input style={{ display: "none" }} id="contained-button-file" type="file" onChange={handleInputChange} />
+
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", flexDirection: "column" }}>
+        <TextField onChange={(e) => { setText(e.target.value) }} label="Enter a name" variant="outlined" sx={{ width: 340 }} />
+        <Stack style={{ gap: 40, marginTop: 40, flexDirection: "row" }}>
+          <div>
+
+
+            <label htmlFor="contained-button-file">
+              <Button variant="contained" color="primary" component="span" sx={{ width: 150, textTransform: "none", fontSize: 18, fontWeight: "bold" }}> Upload </Button>
+            </label>
+
+
+          </div>
+
+
+          <Button variant="contained" color="primary" component="span" onClick={handleButtonClick} sx={{ width: 150, textTransform: "none", fontSize: 18, fontWeight: "bold" }}> Submit </Button>
+        </Stack>
+        <Typography >{filename}</Typography>
+      </div>
+    </>
   );
 }
 
